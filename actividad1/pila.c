ee5abdb52acb56f2a1fb572a58ec1fccbacaf263
Ignacio Ortego <ignacioortego110@gmail.com>
2024-06-28 23:12:44 -0300

    nueva estructura pila, carpeta de tests y makefile

---
+++ actividad1/pila.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "glist.h"
+#include "pila.h"
+
+Pila pila_crear() {
+  return glist_crear();
+}
+
+void pila_destruir(Pila pila, FuncionDestructora destroy) {
+  glist_destruir(pila, destroy);
+}
+
+int pila_es_vacia(Pila pila) {
+  return glist_vacia(pila);
+}
+
+void* pila_tope(Pila pila) {
+  if (pila_es_vacia(pila)) {
+    return NULL;  // Devuelve NULL si la pila está vacía
+  }
+  return pila->data;
+}
+
+void pila_apilar(Pila pila, void* dato, FuncionCopia copy) {
+  pila = glist_agregar_inicio(pila, dato, copy);
+}
+
+void pila_desapilar(Pila pila, FuncionDestructora destroy) {
+  if (!pila_es_vacia(pila)) {
+    GNode* aux = pila->next;
+    destroy(pila->data);
+    free(pila);
+    pila = aux;
+  }
+}
+
+void pila_imprimir(Pila pila, FuncionVisitante visit) {
+  glist_recorrer(pila, visit);
+}
